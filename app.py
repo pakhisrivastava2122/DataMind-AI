0d7f683bb6850d5d56f61b69960ac9b854737862
pakhisrivastava2122 <pakhisrivastava2122@gmail.com>
2026-05-19 21:40:41 +0530

    Fixed Gemini SDK model name

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.set_page_config(
 # ---- GEMINI SETUP ----
 # genai.configure(api_key="AIzaSyBepf_uGWiU1P6GnPGROytTz5JnGn1njxg")
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model_ai = genai.GenerativeModel("gemini-1.5-pro")
+model_ai = genai.GenerativeModel("models/gemini-1.5-flash")
 
 # ---- HELPER FUNCTIONS ----
 def get_ai_response(prompt):
